cd3ea554b68ff5dab2359fa0ad32e30c6066d7b7
ion-golovco <gaminglions2@gmail.com>
2023-02-25 11:53:49 +0200

    Header

--- src/js/eventListener.js
+++ src/js/eventListener.js
@@ -1,38 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+    const burger = document.getElementById("hamburger-button")
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
+    const menu = document.getElementById("mobile-menu")
+
+    const toggleMenu = ()=>{
+        menu.classList.toggle('hidden')
     }
 
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+    burger.addEventListener('click', toggleMenu)
+    menu.addEventListener('click', toggleMenu)
 });
