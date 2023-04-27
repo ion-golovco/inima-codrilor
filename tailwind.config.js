d1e30dc11d33bacd91d15301cab294a30c23e671
ion-golovco <gaminglions2@gmail.com>
2023-04-27 15:16:46 +0300

    new start

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
     theme: {
       colors: {
         transparent:'transparent',
-        black:'#010400',
-        primary:'#a3d881',
-        back:'#FAD275',
+        black:'#0B0D07',
+        primary:'#98C05E',
+        back:'#f3f3f3',
         white:'#ffffff',
         red:'#f6baa6'
     }
